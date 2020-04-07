d0e4dae926bc25bd85a9be05b611ff236271440e
Maximiliano Greco <mgreco@ets.es>
2020-04-07 07:41:26 +0200

    Add pandas loc

--- pandas/loc.py
+++ pandas/loc.py
@@ -2,5 +2,6 @@ import pandas as pd
 import numpy as np
 
 
-df = pd.DataFrame(np.random.rand(10), index=pd.date_range("20180101", periods=10))
-df.loc["01-01"]
+df = pd.DataFrame(np.random.rand(10), index=pd.date_range("20180101", periods=10, freq="H"))
+df.loc["01:00:00"]
+df
